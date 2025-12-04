e1793dd137e887824b30b946af22759610c90499
micalara <micaela.engene@gmail.com>
2025-12-04 14:25:09 -0300

    logos y modificacion visual en resenas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 /* Variables */
-let nombre = "Maria";
+/*let nombre = "Maria";
 console.log(nombre);
+
+let apellido = "Lara";
+console.log(apellido);
+
+let edad = parseInt(prompt("Ingrese su edad: "));
+let calcularEdad = edad + 2;
+
+console.log("Hola mi nombre es "+ nombre + " y mi apellido es " + apellido + ", tengo "+ edad + " anios.");
+console.log(`Hola mi nombre es ${nombre}, mi apellido ${apellido} y tengo ${calcularEdad} anios.`);*/
